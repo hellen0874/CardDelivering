0075913a54a84c25fcb0b0479fff4832740b1ce3
Elena Musinova <en.musinova@gmail.com>
2023-08-31 02:34:46 +0300

    CardDeliveringTest is changed

--- src/test/CardDeliveringTest.java
+++ src/test/CardDeliveringTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -20,18 +21,23 @@ import java.util.List;
 
 public class CardDeliveringTest {
     // Задание 1
-    @Test
-    void shouldPassPositiveTest() {
+    private String generateDate(int addDays, string pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test  // Не могу понять, почему аннотация не подсвечивается.  Уже все подряд импортировала, тест все равно не запускается.
+
+    public void shouldPassPositiveTest() {
         open("http://localhost:9999/");
-        $$(.tab-item);
         $("[data-test-id='city']").setValue("Архангельск");
+        String currentDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id='date'] input").sendKeys(currentDate + 2);
+        $("[data-test-id='date'] input").sendKeys(currentDate);
         $("[data-test-id='name']").setValue("Елена Смирнова-Кузнецова");
         $("[data-test-id='phone']").setValue("+79007777777");
         $("[data-test-id='agreement']").click();
         $("button").click();
-        $(withText(Успешно!)).shoulBe(visible, Duration.ofSeconds(15));
-        $("[data-test-id='agreement']").shoulHave(currentDate + 2);
+        $(withText(Успешно !)).shoulBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id='agreement']").shoulHave(currentDate);
     }
 }
